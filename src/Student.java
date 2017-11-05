8f6a17a98243d6121b751c644e6b6c5e74dd6ba8
tyuyuiertuio <32708495+tyuyuiertuio@users.noreply.github.com>
2017-11-05 22:41:28 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,15 @@
 public class Student {
-    public String Id, first, last;
-    public int grade;
+    public String Id, first, last, course;
+    public double grade;
+    public int year;
 
-    public Student(String Id,String first,String last,int grade){
+    public Student(String Id,String first,String last,double grade,String course,int year){
         this.Id = Id;
         this.first = first;
         this.last = last;
         this.grade = grade;
+        this.course = course;
+        this.year = year;
     }
 
     public String getFirst(){
@@ -14,14 +17,14 @@ public class Student {
     }
 
     public String getId(){
-        return Id
+        return Id;
     }
 
     public String getLast(){
         return last;
     }
 
-    public int getGrade(){
+    public double getGrade(){
         return grade;
     }
 
@@ -29,7 +32,7 @@ public class Student {
         this.first = first;
     }
 
-    public void setGrade(int grade) {
+    public void setGrade(double grade) {
         this.grade = grade;
     }
 
@@ -39,5 +42,26 @@ public class Student {
 
     public void setLast(String last) {
         this.last = last;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
