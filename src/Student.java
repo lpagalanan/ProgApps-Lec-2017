4865b131b0ffbda15ffdbc704d2c4d5b7dbbaa13
tyuyuiertuio <32708495+tyuyuiertuio@users.noreply.github.com>
2017-11-05 22:19:37 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,43 @@
-class Student {
-    public String ID;
-    public String firstName;
-    public String lastName;
+public class Student {
+    public String Id, first, last;
     public int grade;
-    
+
+    public Student(String Id,String first,String last,int grade){
+        this.Id = Id;
+        this.first = first;
+        this.last = last;
+        this.grade = grade;
+    }
+
+    public String getFirst)(){
+        return first;
+    }
+
+    public String getId(){
+        return Id
+    }
+
+    public String getLast(){
+        return last;
+    }
+
+    public int getGrade(){
+        return grade;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
 }
