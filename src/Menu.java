87478369f4937640de4522f6bdb92c6c9695d34b
tyuyuiertuio <32708495+tyuyuiertuio@users.noreply.github.com>
2017-11-06 16:08:09 +0800

    Create Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Mon Nov 06 14:35:32 SGT 2017
+ */
+
+
+
+/**
+ * @author Edson Florendo
+ */
+public class Menu extends JFrame {
+    public Menu() {
+        initComponents();
+    }
+
+    private void button1ActionPerformed(ActionEvent e) {
+        Show1 a = new Show1();
+        a.setVisible(true);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Edson Florendo
+        label1 = new JLabel();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        //---- label1 ----
+        label1.setText("Class record");
+
+        //---- button1 ----
+        button1.setText("Show Students");
+        button1.addActionListener(e -> button1ActionPerformed(e));
+
+        //---- button2 ----
+        button2.setText("Record");
+
+        //---- button3 ----
+        button3.setText("Add a new Activity");
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addComponent(button1, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(button2, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(button3)
+                            .addGap(0, 0, Short.MAX_VALUE)))
+                    .addContainerGap())
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(label1)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button1, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button2, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button3, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
+                    .addContainerGap(11, Short.MAX_VALUE))
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Edson Florendo
+    private JLabel label1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+}
